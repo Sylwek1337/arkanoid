43a3d497bc3df5b2abe97e948b127fc460b4a5ea
Sylwek <sylwek.duchnik@gmail.com>
2021-03-11 16:31:36 +0100

    inicjalizacja projektu

---
+++ arkanoid/src/module-info.java
@@ -0,0 +1,2 @@
+module pl.kielce.tu.arkanoid {
+}
---
+++ arkanoid/src/pl/kielce/tu/arkanoid/Arkanoid.java
@@ -0,0 +1,10 @@
+package pl.kielce.tu.arkanoid;
+
+public class Arkanoid {
+
+	public static void main(String[] args) {
+		System.out.print("test");
+
+	}
+
+}
